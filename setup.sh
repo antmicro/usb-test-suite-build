eec321f53d51eac823f0bd29421b7bafa5e8c770
Jędrzej Boczar <jboczar@antmicro.com>
2020-01-22 12:47:48 +0100

    download sdcc toolchain in setup script

--- setup.sh
+++ setup.sh
@@ -22,6 +22,11 @@ wget https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.1.0-2019.
 tar -xvf riscv64-unknown-elf-gcc-8.1.0-2019.01.0-x86_64-linux-ubuntu14.tar.gz
 rm riscv64-unknown-elf-gcc-8.1.0-2019.01.0-x86_64-linux-ubuntu14.tar.gz
 
+# sdcc toolchain for FX2
+wget "https://sourceforge.net/projects/sdcc/files/sdcc-linux-amd64/3.9.0/sdcc-3.9.0-amd64-unknown-linux2.5.tar.bz2"
+tar xjf "sdcc-3.9.0-amd64-unknown-linux2.5.tar.bz2"
+rm "sdcc-3.9.0-amd64-unknown-linux2.5.tar.bz2"
+
 # fix script permissions of hex2bin module
 cd "$VIRTUAL_ENV/bin"
 chmod u+x bin2hex.py hex2bin.py hex2dump.py hexdiff.py hexinfo.py hexmerge.py || true
